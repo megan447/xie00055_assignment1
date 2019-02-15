a64226b8a91844154acb81e3035ae5c3bbf9be45
Lu Xie <xiemegan447@gmail.com>
2019-02-15 10:50:02 -0500

    assignment1 finished

---
+++ project/src/components/ClaimRequestForm/ClaimRequestForm.js
@@ -0,0 +1,38 @@
+export default {
+  name: 'ClaimRequestForm',
+  data () {
+    return {
+      input10: '',
+      input11: '',
+      input12: '',
+      textarea2: '',
+      textarea3: '',
+      checked: true,
+      dialogVisible: false
+    }
+  }
+  // methods: {
+//     input10:function(){
+//         if(this.name=""){
+//             this.msgame = "You have to type a First name";
+//         }else{
+//             this.msgame ="";
+//         }
+//     },
+//     input11:function(){
+//       if(this.name=""){
+//           this.msgame = "You have to type a First name";
+//       }else{
+//           this.msgame ="";
+//       }
+//   },
+//     input12:function(){
+// var regEmail=/^[A-Za-zd]+([-_.][A-Za-zd]+)*@([A-Za-zd]+[-.])+[A-Za-zd]{2,5}$/;
+// if(this.email==''){
+//   this.msgmail="请输入邮箱";
+// }else if(!regEmail.test(this.email)){
+//   this.msgmail="邮箱格式不正确";
+//   }
+// }
+
+}
---
+++ project/src/components/ClaimRequestForm/ClaimRequestForm.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="hello">
+    <el-form ref="form" :model="form" label-width="120px">
+      <el-form-item label="First name">
+        <el-input placeholder="First name" v-model="input10" clearable></el-input>
+      </el-form-item>
+      <el-form-item label="Last name">
+        <el-input placeholder="Last name" v-model="input11" clearable></el-input>
+      </el-form-item>
+      <el-form-item label="Email">
+        <el-input placeholder="Email address" v-model="input12" clearable></el-input>
+      </el-form-item>
+      <el-form-item label="Subject">
+        <el-input type="textarea" autosize placeholder="Subject" v-model="textarea2"></el-input>
+      </el-form-item>
+      <div style="margin: 20px 0;"></div>
+      <el-form-item>
+        <el-input
+          type="textarea"
+          :autosize="{ minRows: 2, maxRows: 4}"
+          placeholder="Input content"
+          v-model="textarea3"
+        ></el-input>
+      </el-form-item>
+      <!-- `checked` should be true or false -->
+      <el-checkbox v-model="checked">Agreement</el-checkbox>
+      <el-row>
+        <el-button type="primary" @click="dialogVisible = true
+        ">Submit</el-button>
+      </el-row>
+      <el-dialog
+        title="Confirm information"
+        :visible.sync="dialogVisible"
+        width="30%"
+        :before-close="handleClose"
+      >
+        <div class="confirmation">First name: {{input10}}</div>
+        <div class="confirmation">Last name: {{input11}}</div>
+        <div class="confirmation">Email: {{input12}}</div>
+        <div class="confirmation">Subject: {{textarea2}}</div>
+        <div class="confirmation">Content: {{textarea3}}</div>
+        <span slot="footer" class="dialog-footer">
+          <el-button @click="dialogVisible = false">Cancel</el-button>
+          <el-button type="primary" @click="dialogVisible = false">Confirm</el-button>
+        </span>
+      </el-dialog>
+    </el-form>
+  </div>
+</template>
+
+<script src='./ClaimRequestForm.js'>
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style  src='./ClaimRequestForm.css' scoped>
+</style>
